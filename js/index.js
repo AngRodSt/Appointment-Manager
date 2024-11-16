bf2e28ef773dda0d504c39553f78fedf1073381e
AngRod <sthefanyangeles00@gmail.com>
2024-11-16 07:54:54 -0400

    Fix(responsive): responsive design issues and add new modules

--- js/index.js
+++ js/index.js
@@ -73,13 +73,13 @@ class Appointment {
             <p class="uppercase mb-2 text-sm font-bold text-gray-500">Symptoms: <span class=" text-black ">${symptoms}</span></p>
             `
             const btnEdit = document.createElement('button');
-            btnEdit.classList.add('py-2', 'px-10', 'bg-indigo-600', 'hover:bg-indigo-700', 'text-white', 'font-bold', 'uppercase', 'rounded-lg', 'flex', 'items-center', 'gap-2');
+            btnEdit.classList.add('py-2', 'px-10', 'bg-indigo-600', 'hover:bg-indigo-700', 'text-white', 'font-bold', 'uppercase', 'rounded-lg', 'flex', 'items-center', 'text-center','gap-2', 'sm:w-full');
             btnEdit.innerHTML = 'Editar <svg fill="none" class="h-5 w-5" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path></svg>'
             const clone = structuredClone(appointment)
             btnEdit.onclick = () => {loadEdit(clone)}
 
             const btnDelete = document.createElement('button');
-            btnDelete.classList.add('py-2', 'px-10', 'bg-red-600', 'hover:bg-red-700', 'text-white', 'font-bold', 'uppercase', 'rounded-lg', 'flex', 'items-center', 'gap-2');
+            btnDelete.classList.add('py-2', 'px-10', 'bg-red-600', 'hover:bg-red-700', 'text-white', 'font-bold', 'uppercase', 'rounded-lg', 'flex', 'items-center', 'justify-center', 'gap-2', 'sm:w-full');
             btnDelete.innerHTML = 'Eliminar <svg fill="none" class="h-5 w-5" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>'
             btnDelete.onclick = ()=>{
                 this.deleteAp(clone)
@@ -92,7 +92,7 @@ class Appointment {
             }
 
             const containerButton = document.createElement('DIV');
-            containerButton.classList.add('flex', 'justify-between', 'mt-5', 'gap-3');
+            containerButton.classList.add('md:flex-row' , 'flex', 'flex-col','flex-1', 'md:justify-between', 'mt-5', 'gap-3');
 
             containerButton.appendChild(btnEdit);
             containerButton.appendChild(btnDelete);
